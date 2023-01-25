620b379e61e4f9f7d52cc756eef8677e9e2bd262
Cuji12 <cu.janeway@gmail.com>
2023-01-25 20:32:37 +0000

    Change index image, remove old image upload code.

--- resources/views/site/about-this-site.blade.php
+++ resources/views/site/about-this-site.blade.php
@@ -14,15 +14,15 @@
         <div class="content-center p-8 sm:flex sm:flex-wrap sm:px-0 sm:flex-col">
             <h2 class="mb-1 text-lg font-bold sm:w-9/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 4xl:w-3/12 4xl:text-xl">Codebase and CMS</h2>
             <p class="mb-6 sm:w-9/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 4xl:w-3/12 4xl:text-lg">
-                This portfolio is a fairly simple build using Laravel with an admin side setup to easily update the website. 
-                It's all hosted in a public repository so by all means feel free to click the link below and check out how it's written. 
-                If you're having a nosey and curious about deploying Docker images and transmorgifying them to Firecracker VMs in Fly, go check 
+                This portfolio is a fairly simple build using Laravel with an admin side setup to easily update the website.
+                It's all hosted in a public repository so by all means feel free to click the GitHub link below and check out how it's written.
+                If you're having a nosey and curious about deploying Docker images and transmogrifying them to Firecracker VMs in Fly, go check
                 out their website linked below &#128578;.
             </p>
             <h3 class="mb-1 text-lg font-bold sm:w-9/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 4xl:w-3/12 4xl:text-xl">More details:</h3>
             <ul class="list-disc list-inside marker:text-green sm:w-9/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 4xl:w-3/12 4xl:text-lg">
                 <li>Designed in AdobeXD</li>
-                <li>Code on <a class="underline hover:text-green" href="https://github.com/Cuji12/laravel-cujaneway">Github</a>
+                <li>Code on <a class="underline hover:text-green" href="https://github.com/Cuji12/laravel-cujaneway">GitHub</a>
                 </li>
                 <li>Built with Laravel</li>
                 <li>Deployed with <a class="underline hover:text-green" href="https://fly.io/">Fly.io</a> using Firecracker microVMs</li>
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -13,7 +13,7 @@
         <div class="relative flex flex-wrap w-11/12 p-6 py-24 rounded-md bg-pink xl:p-20 pb-96 xl:w-10/12 2xl:w-9/12 3xl:w-4/6 xl:py-44 xl:items-end lg:w-9/12">
             <h1 class="absolute text-3xl font-bold tracking-widest green-highlight xl:text-6xl xl:top-20 xl:n-left-24 top-10 n-left-4 md:text-4xl md:n-left-13">Cu Janeway</h1>
             <div class="relative flex flex-wrap xl:flex-nowrap">
-                <img class="mb-6 xl:w-80 3xl:w-92 xl:mb-0 sm:w-72 5xl:w-112" src="/images/me.jpg" alt="Picture of me during a hike."/>
+                <img class="mb-6 xl:w-80 3xl:w-92 xl:mb-0 sm:w-72 5xl:w-112" src="/images/me.png" alt="Picture of me during a hike."/>
                 <p class="top-0 w-full mb-2 font-medium tracking-wider text-green xl:absolute xl:text-2xl xl:rotate-90 xl:t-origin-tl xl:n-right-88 md:text-xl 5xl:text-4xl 5xl:n-right-124">Web Developer</p>
             </div>
             <p class="green-highlight xl:max-w-128 xl:text-xl xl:ml-16 md:text-lg 5xl:text-2xl">

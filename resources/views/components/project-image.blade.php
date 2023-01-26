d69dbfdf8bfc42f8da8a997f48cfd5231fd19930
Cuji12 <cu.janeway@gmail.com>
2023-01-26 10:47:02 +0000

    Add keyframe animation.

--- resources/views/components/project-image.blade.php
+++ resources/views/components/project-image.blade.php
@@ -4,27 +4,41 @@
 ])
 <div class="absolute hidden w-screen h-screen bg-red">
 </div>
-
- <img
-    src="{{ $image }}"
+<div
     x-data="{ enlarge: false }"
-    x-on:click="enlarge = ! enlarge; console.log(enlarge)"
+    @class([
+        'md:w-9/12',
+        'xl:w-5/12',
+        '2xl:w-3/12' => $project->portrait_images,
+        '3xl:w-2/12' => $project->portrait_images,
+ ])>
+    <img
+    src="{{ $image }}"
+    x-on:click="enlarge = ! enlarge"
     x-on:click.outside="enlarge = false"
-    x-bind:class="{'scale-test z-10 absolute': enlarge}"
+    x-bind:class="{'scale-test': enlarge}"
 
     @class([
         'cursor-pointer',
         'relative',
-        'transition',
-        'duration-300',
-        'ease-out',
-        'transform',
         'w-full',
         'rounded-md',
-        'md:w-9/12',
-        'xl:w-4/12',
-        '2xl:w-3/12' => ! $project->portrait_images,
-        '3xl:w-2/12' => ! $project->portrait_images,
         'scale-50' => ! 'enlarge'
     ])
     />
+
+    <div
+        x-bind:class="enlarge ? 'inline-block' : ''"
+        @class([
+              'hidden',
+              'relative',
+              'w-full',
+              'rounded-md',
+              'md:w-9/12',
+              'xl:w-5/12',
+              '2xl:w-3/12' => $project->portrait_images,
+              '3xl:w-2/12' => $project->portrait_images,
+          ])
+    >
+    </div>
+</div>
